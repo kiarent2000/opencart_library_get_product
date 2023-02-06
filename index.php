c456cd931579d64a7df928328b08925820a035f7
Sergiy Soroka <vesper7777777@gmai.com>
2023-02-06 14:11:45 +0200

    10

--- index.php
+++ index.php
@@ -20,36 +20,4 @@ foreach($items as $item)
     print_r($product);
 }
 
-//print_r($items);
-
-
-//$item = (new getItem())->get($con, 122778);
-
-//print_r($item);
-
-
-/*
-##########################################################################
-include('item.php');  // загружаем тестовый продукт
-
-
-$product_id=(new CheckProduct(778))->check($dbh); //проверяем наличие продукта по полю 'sku'
-
-if(!$product_id) // добавляем новый продук
-{
-    $item = new Item($test_item); // получаем экземляр класса единицы продукта
-    $result =  (new AddProduct($dbh))->add($item);
-    if($result===200){echo "Продукт успешно добавлен!";}else{print_r($result);}
-
-} else {    // обновляем существующий продукт
-    echo "Обновляем продукт";
-}
-*/
-
-
-
-
-
-
-
 ?>
