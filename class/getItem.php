26a003970f5ade9151feb29a9d27d0d0992ec7aa
Sergiy Soroka <vesper7777777@gmai.com>
2023-02-06 14:11:11 +0200

    9

--- class/getItem.php
+++ class/getItem.php
@@ -1,27 +1,25 @@
 <?php
 class getItem
 {
-    public $product_id;
-    
-    public function __construct($product_id)
-    {
-        $this->product_id=$product_id;
-    }
-
-    public function get($db)
+    public function get($db, $product_id)
     {
         $sql = 'SELECT model, a.sku, a.jan, a.quantity, a.stock_status_id, a.image, a.manufacturer_id, a.price, a.date_modified, b.name, b.description, (SELECT d.category_id FROM '. DB_PREFIX.'_product_to_category d WHERE  a.product_id=d.product_id) as category,
         (SELECT f.name  FROM '. DB_PREFIX.'_category_description f WHERE f.category_id=category AND f.language_id=3) as category_name,
         (SELECT e.name  FROM '. DB_PREFIX.'_manufacturer e WHERE e.manufacturer_id=a.manufacturer_id) as manufacturer
         FROM '. DB_PREFIX.'_product a LEFT JOIN '. DB_PREFIX.'_product_description b ON b.product_id=a.product_id AND b.language_id=3
-        WHERE a.product_id = '.$this->product_id;
-        
-    
-        
-       // echo $sql.'<br>';
-
+        WHERE a.product_id = '.$product_id;
+       
 		$result = mysqli_query($db, $sql);
 		$row = mysqli_fetch_assoc($result);
 		return $row;
+    }
+
+    public function getAll($db)
+    {
+        $sql = 'SELECT  `product_id`    
+        FROM '. DB_PREFIX.'_product ORDER BY `product_id` DESC LIMIT 10';
+		$result = mysqli_query($db, $sql);
+		while( $row = mysqli_fetch_assoc($result)){$arr[] = $row;}
+		return $arr;
     }
 }
--- index.php
+++ index.php
@@ -5,14 +5,27 @@ spl_autoload_register(function ($class) {
     include __DIR__ . '/class/'.$class.'.php';
 });
 
+$it=new getItem();
+
 include(__DIR__.'/config.php');
 $con = (new DB())->connect();
 
 
+$items = $it->getAll($con);
 
-$item = (new getItem(122778))->get($con);
+foreach($items as $item)
+{
+    $product = $it->get($con, $item['product_id']);
+    echo $item['product_id'];
+    print_r($product);
+}
 
-print_r($item);
+//print_r($items);
+
+
+//$item = (new getItem())->get($con, 122778);
+
+//print_r($item);
 
 
 /*
