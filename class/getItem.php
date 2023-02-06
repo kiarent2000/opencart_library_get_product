dc42eec1364e693a6132875989941d92f524581c
Sergiy Soroka <vesper7777777@gmai.com>
2023-02-06 13:46:56 +0200

    6

--- class/getItem.php
+++ class/getItem.php
@@ -10,10 +10,12 @@ class getItem
 
     public function get($db)
     {
-        $sql = 'SELECT model, a.sku, a.jan, a.quantity, a.stock_status_id, a.image, a.manufacturer_id, a.price, a.date_modified, b.name, b.description FROM '. DB_PREFIX.'_product a LEFT JOIN '. DB_PREFIX.'_product_description b ON b.product_id=a.product_id AND b.language_id=3
-         WHERE a.product_id = '.$this->product_id;
+        $sql = 'SELECT model, a.sku, a.jan, a.quantity, a.stock_status_id, a.image, a.manufacturer_id, a.price, a.date_modified, b.name, b.description, (SELECT d.category_id FROM '. DB_PREFIX.'_product_to_category d WHERE  a.product_id=d.product_id) as category FROM '. DB_PREFIX.'_product a LEFT JOIN '. DB_PREFIX.'_product_description b ON b.product_id=a.product_id AND b.language_id=3
+        WHERE a.product_id = '.$this->product_id;
         
-        echo $sql;
+    
+        
+        echo $sql.'<br>';
 
 		$result = mysqli_query($db, $sql);
 		$row = mysqli_fetch_assoc($result);
